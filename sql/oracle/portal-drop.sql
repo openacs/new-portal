a29ae3ae8c4f9cc27821f32de055dbca6214a79c
Yonatan Feldman <yon@milliped.com>
2001-09-28 00:59:11 +0000

    checking in

---
+++ sql/oracle/portal-drop.sql
@@ -0,0 +1,11 @@
+--
+-- The New Portal Package
+-- copyright 2001, OpenForce, Inc.
+-- distributed under the GNU GPL v2
+--
+-- Arjun Sanyal (arjun@openforce.net)
+-- $Id$
+--
+@@ api-drop.sql
+@@ portal-core-drop.sql
+@@ objects-drop.sql
