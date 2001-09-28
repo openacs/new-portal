8a65113ccdd2dc3a393bf63905e7a0e4b8ac4679
Yonatan Feldman <yon@milliped.com>
2001-09-28 00:41:10 +0000

    Initial revision

---
+++ sql/oracle/portal-core-drop.sql
@@ -0,0 +1,21 @@
+-- The New Portal Package
+-- copyright 2001, OpenForce, Inc.
+-- distributed under the GNU GPL v2
+--
+-- arjun@openforce.net
+-- 
+-- $Id$
+
+-- Reverse order from the creation script
+
+drop table portal_mime_types;
+drop table portal_data_types;
+drop table portal_datasources;
+drop table portal_datasource_default_parameters;
+drop table portal_layouts;
+drop table portal_supported_regions;
+drop table portal_element_themes;
+drop table portal_element_map;
+drop table portal_element_parameters;
+drop table portals;
+
