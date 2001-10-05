ceeda53d4763da9d6390f543862a171f547e7b2a
Yonatan Feldman <yon@milliped.com>
2001-10-05 00:15:07 +0000

    updated api

--- www/show-portal.tcl
+++ www/show-portal.tcl
@@ -10,12 +10,8 @@ ad_page_contract {
     portal_id:naturalnum,notnull
 }
 
-set master_template [ad_parameter master_template]
-
-set element_list [portal_setup_element_list $portal_id]
-set element_src [portal_setup_element_src $portal_id]
-array set portal [portal_render_portal $portal_id]
 
 
-ad_return_template
+array set portal [portal::render_portal $portal_id]
+
 
