ff3b241d1b01dc9d3847e3e7c2adaeaf381b0f2d
Yonatan Feldman <yon@milliped.com>
2001-10-23 19:48:50 +0000

    adding portal-ae stuff to this page

--- www/element-layout.tcl
+++ www/element-layout.tcl
@@ -7,11 +7,47 @@ ad_page_contract {
     @creation-date 12/6/2000
     @cvs-id $Id$
 } {
-    portal_id:naturalnum,notnull,object_write
+    portal_id:naturalnum,notnull
 }
 
-set user_id [ad_conn user_id]
+# Check for permission
+ad_require_permission $portal_id portal_read_portal
+ad_require_permission $portal_id portal_edit_portal
+
+# Set up some template vars
 set master_template [ad_parameter master_template]
+
+# get the layouts
+set layout_count 0
+template::multirow create layouts layout_id name description filename resource_dir checked
+
+db_foreach get_layouts "
+select 
+layout_id, 
+name, 
+description, 
+filename, 
+resource_dir, 
+' ' as checked
+from portal_layouts
+order by name" {
+
+    set resource_dir "$resource_dir"
+
+    # this is evil and broken.  
+    # I should be able to pass it a list, straight from db_list.
+    template::multirow append layouts $layout_id $name $description $filename $resource_dir $checked
+    incr layout_count
+}
+
+set title "Edit Your Portal"
+
+# the portal we're editing exists.  Return it.
+db_1row get_portal "select
+name,
+layout_id
+from portals
+where portal_id = :portal_id"
 
 # get the portal.
 db_1row select_portal {
@@ -31,6 +67,6 @@ foreach region [ portal::get_regions $portal(layout_id) ] {
 }
 
 set element_list [array get fake_element_ids]
-set element_src "[portal::portal_path]/www/place-element"
+set element_src "[portal::www_path]/place-element"
 
 ad_return_template
