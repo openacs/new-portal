e714d5158b3f0c5e552c15dce990806a49f0a70e
Roel Canicula <roelmc@info.com.ph>
2005-10-03 18:07:17 +0000

    Use the portal theme instead of a raw themplate for admin portals

--- www/render_styles/all_in_one/render-element.tcl
+++ www/render_styles/all_in_one/render-element.tcl
@@ -26,12 +26,12 @@ ad_page_contract {
     region:onevalue
 }
 
-if { [catch {set element_data [portal::evaluate_element_raw $element_id] }  errmsg ]  } { 
+if { [catch {set element_data [portal::evaluate_element -portal_id $portal_id -edit_p f $element_id $theme_id] }  errmsg ]  } { 
     # An uncaught error happened when trying to evaluate the element.
     # If the error is in the element's "show" proc, the error will
     # be shown in the content of the portlet. This is for errors other
     # than with the "show" proc. 
-    ns_log error "\n\n *** Error in portal/www/render_sytles/all_in_one/render-element.tcl \n Uncaught exception when calling portal::evaluate_element \n with element_id $element_id\n\n"
+    ns_log error "\n\n *** Error in portal/www/render_sytles/all_in_one/render-element.tcl \n Uncaught exception when calling portal::evaluate_element \n with element_id $element_id\n\n ERROR $errmsg"
     ad_return -code error
 } else {
     # all is ok
