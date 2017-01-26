c360dadcbcc1d01aabb9fe3b3d04ff5c9800ec4d
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2017-01-26 09:30:44 +0000

    - tighten page contracts

--- www/admin/portal-show.tcl
+++ www/admin/portal-show.tcl
@@ -25,10 +25,10 @@ ad_page_contract {
 } {
     {referer:notnull}
     portal_id:naturalnum,notnull
-    {page_num 0}
+    {page_num:naturalnum,notnull 0}
 }
 
-set name "[portal::get_name $portal_id]"
+set name [portal::get_name $portal_id]
 set html "[portal::navbar -portal_id $portal_id] [portal::render -page_num $page_num $portal_id]"
 
 
