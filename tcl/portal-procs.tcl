f7b41efb13b283648bc0b61f9b7c6ad9c2c895f9
Antonio Pisano <antonio@elettrotecnica.it>
2020-09-01 17:24:47 +0000

    Improve sql portability

--- tcl/portal-procs.tcl
+++ tcl/portal-procs.tcl
@@ -2489,7 +2489,16 @@ ad_proc portal::portlet_visible_p {
 
 } {
     set ds_id [get_datasource_id $portlet_name]
-    return [db_string portlet_visible {}]
+    return [db_string portlet_visible {
+        select case when exists (select 1
+                       from portal_element_map,
+                       portal_pages
+                       where portal_pages.portal_id = :portal_id
+                       and portal_element_map.datasource_id = :ds_id
+                       and portal_element_map.page_id = portal_pages.page_id
+                       and portal_element_map.state <> 'hidden') then 1 else 0 end
+        from dual
+    }]
 }
 
 # Local variables:
