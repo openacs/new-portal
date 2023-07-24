1e25f62c64e9d027c6081ef838be039f064d3824
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2023-07-24 13:08:29 +0000

    skip "-url" in "export_vars -url" since it is the default

--- tcl/portal-procs.tcl
+++ tcl/portal-procs.tcl
@@ -2498,7 +2498,7 @@ ad_proc portal::dimensional {
             if {($option_val eq $thisoption_name && !$link_all) || !$thisoption_link_p} {
                 append html "${pre_selected_td_html}${pre_html}${thisoption_value}${post_selected_html}\n"
             } else {
-                set href $url?[export_vars -url -set $options_set [list [list $option_key $thisoption_name]]]
+                set href [export_vars -base $url -set $options_set [list [list $option_key $thisoption_name]]]
                 append html [subst {
                     ${pre_td_html}<a href="[ns_quotehtml $href]">${pre_html}${thisoption_value}${post_html}
                 }]
